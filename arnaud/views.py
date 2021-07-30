7a8391de2c85cbdf98c68db64723a053e4ad351f
Sebastien Pro <spro@webgeoservices.com>
2021-07-30 16:07:30 +0200

    Send Succed

--- arnaud/views.py
+++ arnaud/views.py
@@ -19,18 +19,22 @@ def generate_thumbnail(image_path):
     image = PilImage.open(image_path)
     image_name = os.path.basename(image_path)
     MAX_SIZE = (200, 200)
-    orientation_item = None
-    for orientation in ExifTags.TAGS.keys():
-        if ExifTags.TAGS[orientation] == 'Orientation':
-            orientation_item = orientation
-    exif = dict(image._getexif().items())
 
-    if exif[orientation_item] == 3:
-        image = image.rotate(180, expand=True)
-    elif exif[orientation_item] == 6:
-        image = image.rotate(270, expand=True)
-    elif exif[orientation_item] == 8:
-        image = image.rotate(90, expand=True)
+    try:
+        orientation_item = None
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == 'Orientation':
+                orientation_item = orientation
+        exif = dict(image._getexif().items())
+
+        if exif[orientation_item] == 3:
+            image = image.rotate(180, expand=True)
+        elif exif[orientation_item] == 6:
+            image = image.rotate(270, expand=True)
+        elif exif[orientation_item] == 8:
+            image = image.rotate(90, expand=True)
+    except:
+        pass
 
     image.thumbnail(MAX_SIZE)
     image.save(os.path.join(settings.MEDIA_ROOT,'thumbnail',image_name))
@@ -119,6 +123,7 @@ def detail(request, image_id, user):
 
 @csrf_exempt
 def add(request):
+    send  = False
     if request.method == 'POST' and request.FILES['photo']:
         photo = request.FILES['photo']
         fs = FileSystemStorage()
@@ -126,4 +131,5 @@ def add(request):
         saved_image = Image(image_name=filename)
         saved_image.save()
         generate_thumbnail(fs.path(filename))
-    return HttpResponse(render(request, 'add.html'))
+        send = True
+    return HttpResponse(render(request, 'add.html', context={'send': send}))
