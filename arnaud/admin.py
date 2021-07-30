7d38eb108644627d4364089ff0262f08b31e314b
Sebastien Pro <spro@webgeoservices.com>
2021-07-30 17:21:17 +0200

    Add admin page

--- arnaud/admin.py
+++ arnaud/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from arnaud.models import Image, Comment, Person
+
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Person)
--- arnaud/models.py
+++ arnaud/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Image(models.Model):
     image_name = models.CharField(max_length=200)
     persons = models.ManyToManyField(Person, through='Interested')
+
+    def __str__(self):
+        return self.image_name
 
 class Interested(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
@@ -18,3 +24,6 @@ class Comment(models.Model):
     text = models.TextField()
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
